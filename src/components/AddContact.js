2f5d909f62660c242a3c26f134651f7cbe6d28fe
amruta.ks <amruta.ks2022cse@sece.ac.in>
2025-03-27 15:43:59 +0530

    commit

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,30 +1,51 @@
-import { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const AddContact=()=>{
-    const [name,setName]=useState("");
-    const [email,setEmail]=useState("");
+class AddContact extends React.Component {
 
-    const handleChange=()=>{
-        console.log(name);
-        console.log(email);
+
+
+    state = {
+        
+        name: "",
+        email: "",
     }
 
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.name === "" || this.state.email === "") {
+            alert("All the fields are mandatory!");
+            return;
+        }
+        console.log(this.state);
+        this.props.addContactHandler(this.state);
+        this.setState({name: "", email: ""});
+        console.log(this.props);
+        //this.props.history.push("/");
+    }
+    render() {
     return(
         <>
-              <h2 align='center'>Add user</h2>
+        <h2 align='center'>Add user</h2>
         <div class='outer-form'>
-        <form>
+        <Link to="/">
+                    <button className="ui button blue right">Back to ContactList</button>
+
+                    </Link>
+
+                    <form onSubmit={this.handleSubmit }>
             <div class='form-div'>
                 <div class='inside'>
             <label class='form-label'>Name :</label>
-            <input type="text" name="name" class='input-box' placeholder='Name' onChange={(e)=>{setName(e.target.value)}}></input>
+            <input type="text" name="name" class='input-box' placeholder='Name' value={this.state.name} onChange={(e) => this.setState({name: e.target.value}) }></input>
             </div>
             <div class='inside'>
             <label class='form-label' >Email :</label>
-            <input type="text" name="email" class='input-box' placeholder='Email' onChange={(e)=>{setEmail(e.target.value)}}></input>
+            <input type="text" name="email" class='input-box' placeholder='Email' value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}></input>
             </div>
             <div class='button-div'>
-            <button onClick={handleChange}>Add</button>
+            <button >Add</button>
             </div>
             </div>
         </form>
@@ -32,4 +53,6 @@ const AddContact=()=>{
         </>
     )
 }
+}
+
 export default AddContact;
